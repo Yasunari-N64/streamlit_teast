98ba16974b4ff74d4ef72e428e4eea6e366ba075
Ariel Yasunari <yasunari.atenco.u@gmail.com>
2025-07-22 10:58:06 -0600

    añadir las salidas a una tabla con los resultados de todos los intentos

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
+import pandas as pd
 import scipy.stats
 import streamlit as st
 import time
+
+# estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
 
 st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
 
-def toss_coin(n): # función que emula el lanzamiento de una moneda
+def toss_coin(n):
 
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
